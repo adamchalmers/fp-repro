6d24b2e4426e788f1f4ecbb641b9c9bf098ba02e
Adam Chalmers <adam.s.chalmers@gmail.com>
2024-11-17 12:20:45 -0600

    Simplify

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 fn main() {
-    let x_to = 3.0;
-    let from_x = 1.0;
     let angle: f64 = -59.5694837374164;
     let angle_r = angle.to_radians();
-    let x_component = x_to - from_x;
     println!("{angle_r}");
-    let y_component = x_component * f64::tan(angle_r);
+    let y_component = f64::tan(angle_r);
     println!("{y_component}");
 }
